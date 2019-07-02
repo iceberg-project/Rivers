968f7d3b789842fe3a814981a4f270915c921a65
Ioannis Paraskevakos <i.paraskev@rutgers.edu>
2019-07-02 10:51:11 -0400

    multipagetif as a function and discovery python file included

---
+++ src/utils/multipagetiff.m
@@ -0,0 +1,15 @@
+% Author: Samira Daneshgar-Asl
+% License: MIT
+% Copyright: 2018-2019
+
+% when we have an 8-bit image with 3 bands and we want to save it as a multi-page
+
+function multipagetiff(ReadImage, WriteDir)
+
+if ~exist(WriteDir, 'dir')
+    mkdir(WriteDir);
+end
+
+image = geotiffread(ReadImage);
+writeFileName = strcat(WriteDir,'/multipage-',num2str(ReadImage));
+saveastiff(image,writeFileName);
