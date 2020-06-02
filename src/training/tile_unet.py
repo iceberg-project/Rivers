367b644d401bfe76e8ba109c22e3f6e3cc08fa91
Samira Daneshgar Asl <samira@geog.ucsb.edu>
2020-06-02 18:13:00 -0400

    divides a WorldView image into tiles

--- src/training/tile_unet.py
+++ src/training/tile_unet.py
@@ -7,17 +7,10 @@ import os
 import argparse
 from osgeo import gdal
 
-#loading WorldView image 
-def load_image(path):
-    ds = gdal.Open(path) 
-    xsize = ds.RasterXSize    
-    ysize = ds.RasterYSize     
-    return xsize,ysize,ds
- 
 def args_parser():
     parser = argparse.ArgumentParser(description="divides a WorldView image into tiles")
     parser.add_argument('-i', '--input', type=str,
-                        help='Path and Filename of the WorldView Image')
+                        help='Path and name of the WorldView image')
     parser.add_argument('-t', '--tile_size', type=int, default=224,
                         help='Tile size')
     parser.add_argument('-s', '--step', type=int, default=112,
@@ -33,16 +26,12 @@ if __name__ == '__main__':
     if not os.path.exists(out_path):
        os.makedirs(out_path)
  
-    tile_size_x = args.tile_size
-    tile_size_y = args.tile_size
-    step=args.step
- 
-    xsize,ysize,image = load_image(args.input)	
+    image = gdal.Open(args.input)	
 
     k=0 
-    for i in range(0, xsize, step):
-        for j in range(0, ysize, step):
-            com_string = "gdal_translate -of GTIFF -srcwin " + str(i)+ ", " + str(j) + ", " + str(tile_size_x) + ", " + str(tile_size_y) + " " + str(args.input) + " " + str(out_path) + str(k) + ".tif"
+    for i in range(0, image.RasterXSize, args.step):
+        for j in range(0, image.RasterYSize, args.step):
+            com_string = "gdal_translate -of GTIFF -srcwin " + str(i)+ ", " + str(j) + ", " + str(args.tile_size) + ", " + str(args.tile_size) + " " + str(args.input) + " " + str(out_path) + str(k) + ".tif"
             os.system(com_string)
             k+=1
 
