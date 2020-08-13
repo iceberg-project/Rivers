397460fb79b7c5e78e2868eee98ec3c6ac658316
Ioannis Paraskevakos <i.paraskev@rutgers.edu>
2020-08-13 12:26:39 -0400

    package almost there

--- src/iceberg_rivers/search/classification/predict_unet.py
+++ src/iceberg_rivers/search/classification/predict_unet.py
@@ -6,7 +6,7 @@ Copyright: 2019-2020
 import os
 import numpy as np
 import argparse
-from model import *
+from .model import *
 from keras.preprocessing.image import img_to_array
 from osgeo import gdal
 from os import listdir
@@ -19,9 +19,9 @@ def adjustTile(img):
 #loading tiles
 def load_tile(path):
     ds = gdal.Open(path)
-    band3=ds.GetRasterBand(3)
-    band5=ds.GetRasterBand(5)
-    band6=ds.GetRasterBand(6)       
+    band3 = ds.GetRasterBand(3)
+    band5 = ds.GetRasterBand(5)
+    band6 = ds.GetRasterBand(6)       
     img_proj = ds.GetProjection()
     img_geotrans = ds.GetGeoTransform()
     b3,b5,b6 = (band3.ReadAsArray(0,0, ds.RasterXSize, ds.RasterYSize),
@@ -43,6 +43,29 @@ def write_mask(filename,img_proj,img_geotrans,img_data):
     ds.SetGeoTransform(img_geotrans)                          
     ds.GetRasterBand(1).WriteArray(img_data)
     
+def predict_unet():
+
+    model = unet()
+    model.load_weights(args.weights_path)
+    tiles_path = 'image_tiles/' + args.input + '/' 
+    list = os.listdir(tiles_path) 
+    num_tiles = len(list)
+	
+    out_path = 'predicted_tiles/' + args.input + '/'
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
+    
+    for n in range(num_tiles):
+        tile_name = list[n]
+        head, tail = tile_name.split('.tif')
+        proj, geotrans, tile= load_tile(tiles_path + tile_name)
+        array_tile = img_to_array(tile)
+        expand_tile = np.expand_dims(array_tile, axis=0)       	
+        pred = model.predict(expand_tile)
+        for i,item in enumerate(pred):
+            mask = item[:,:,0]
+        write_mask(out_path+ "%s.tif"%head, proj, geotrans, mask)
+
 def args_parser():
     parser = argparse.ArgumentParser(description="predicts tiles")
     parser.add_argument('-i', '--input', type=str,
@@ -76,6 +99,3 @@ if __name__ == '__main__':
         for i,item in enumerate(pred):
             mask = item[:,:,0]
         write_mask(out_path+ "%s.tif"%head, proj, geotrans, mask)
-	
-	
-	
