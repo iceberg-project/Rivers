29881a351782cbaa1f9f1a005686ecdc2a838410
Ioannis Paraskevakos <i.paraskev@rutgers.edu>
2020-08-13 12:29:03 -0400

    predict unet updated for package

--- src/iceberg_rivers/search/classification/predict_unet.py
+++ src/iceberg_rivers/search/classification/predict_unet.py
@@ -5,11 +5,9 @@ Copyright: 2019-2020
 """
 import os
 import numpy as np
-import argparse
 from .model import *
 from keras.preprocessing.image import img_to_array
 from osgeo import gdal
-from os import listdir
 		
 def adjustTile(img):
     if(np.max(img) > 1):
@@ -43,49 +41,15 @@ def write_mask(filename,img_proj,img_geotrans,img_data):
     ds.SetGeoTransform(img_geotrans)                          
     ds.GetRasterBand(1).WriteArray(img_data)
     
-def predict_unet():
+def predict_unet(input, weights_path, output_folder):
 
     model = unet()
-    model.load_weights(args.weights_path)
-    tiles_path = 'image_tiles/' + args.input + '/' 
+    model.load_weights(weights_path)
+    tiles_path = input
     list = os.listdir(tiles_path) 
     num_tiles = len(list)
 	
-    out_path = 'predicted_tiles/' + args.input + '/'
-    if not os.path.exists(out_path):
-        os.makedirs(out_path)
-    
-    for n in range(num_tiles):
-        tile_name = list[n]
-        head, tail = tile_name.split('.tif')
-        proj, geotrans, tile= load_tile(tiles_path + tile_name)
-        array_tile = img_to_array(tile)
-        expand_tile = np.expand_dims(array_tile, axis=0)       	
-        pred = model.predict(expand_tile)
-        for i,item in enumerate(pred):
-            mask = item[:,:,0]
-        write_mask(out_path+ "%s.tif"%head, proj, geotrans, mask)
-
-def args_parser():
-    parser = argparse.ArgumentParser(description="predicts tiles")
-    parser.add_argument('-i', '--input', type=str,
-                            help='Input image tiles folder')
-    parser.add_argument('-w', '--weights_path', type=str,
-                            help='Path to the weights')
-    parser.add_argument('-o', '--output_folder', type=str, default='./',
-                        help='Folder where output masks will be stored')			
-    return parser.parse_args()
-    
-if __name__ == '__main__':
-    args = args_parser()
-
-    model = unet()
-    model.load_weights(args.weights_path)
-    tiles_path = 'image_tiles/' + args.input + '/' 
-    list = os.listdir(tiles_path) 
-    num_tiles = len(list)
-	
-    out_path = 'predicted_tiles/' + args.input + '/'
+    out_path = output_folder
     if not os.path.exists(out_path):
         os.makedirs(out_path)
     
